1bbe5a1b7f795a1e95c5ba2aa203207b5dcb4010
Hamdy Elbatal <elbatalhamdy77@gmail.com>
2021-07-21 17:09:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask_login import LoginManager, login_required, UserMixin, login_user, log
 from urllib.parse import urlparse, urljoin
 import requests, tableauserverclient as TSC
 from secret import SECRET_KEY, TABLEAU_AUTH, USERS_LIST, PASSWORD_LOGIN
-
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 login_manager = LoginManager(app)
